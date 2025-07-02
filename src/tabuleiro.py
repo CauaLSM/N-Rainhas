7c650684b0c0967e98ba43424add5defce6ba0bf
Cauã <caualuis2012@hotmail.com>
2025-07-02 17:40:05 -0300

    funções iniciais da classse tabuleiro e criação de pastas e arquivos modelo

---
+++ src/tabuleiro.py
@@ -0,0 +1,23 @@
+import random
+
+def criar_estado_inicial(n):
+    estado = list(range(n))
+    random.shuffle(estado)
+    return estado
+
+def contar_conflitos(estado):
+    n = len(estado)
+    conflitos = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if estado[i] == estado[j] or abs(estado[i] - estado[j]) == abs(i - j):
+                conflitos += 1
+    return conflitos
+
+def printar_tabuleiro(estado):
+    n = len(estado)
+    for linha in range(n):
+        linha_visual = ['.'] * n
+        linha_visual[estado[linha]] = 'Q'
+        print(' '.join(linha))
+    print()
